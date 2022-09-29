55c371b1f6e41d0fefe08e7cd490cee9a010591d
thaisperlho <carvalho.thais0301@gmail.com>
2022-09-29 10:23:28 -0300

    feat: create Adotante.java

---
+++ CRUD-JAVA/src/br/edu/fatec/encontrandocaramelos/adotante/Adotante.java
@@ -0,0 +1,45 @@
+package br.edu.fatec.encontrandocaramelos.adotante;
+
+public class Adotante {
+
+    //ATRIBUTOS
+    private String nome;
+    private String endereco;
+    private int telefone;
+    private String email;
+
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public int getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void imprimirDados() {
+        System.out.println("Nome: " + getNome());
+        System.out.println("Email: " + getEmail());
+        System.out.println("Telefone: " + getTelefone());
+        System.out.println("Endereço: " + getEndereco() + "\n");
+    }
+
+
+}
